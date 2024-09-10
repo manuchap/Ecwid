68c20e6533e6c253c8d2ff51b283171ab7d4114a
Manu <admin@studiowan.fr>
2024-09-10 21:02:48 +0200

    remove slider button

--- public/javascripts/slides.js
+++ public/javascripts/slides.js
@@ -1,23 +1,3 @@
-// function plusDivs(e)
-// {
-//   showDivs(slideIndex += e)
-// }
-
-// function currentDiv(e)
-// {
-//   showDivs(slideIndex = e)
-// }
-
-// function showDivs(e)
-// {
-//   var s, n = document.getElementsByClassName("mySlides"),
-//     d = document.getElementsByClassName("dots");
-//   for (e > n.length && (slideIndex = 1), e < 1 && (slideIndex = n.length), s = 0; s < n.length; s++) n[s].style.display = "none";
-//   for (s = 0; s < d.length; s++) d[s].className = d[s].className.replace(" w3-white", "");
-//   n[slideIndex - 1].style.display = "block", d[slideIndex - 1].className += " w3-white"
-// }
-// var slideIndex = 1;
-// showDivs(slideIndex)
 var slideIndex = 0;
 carousel();
 
